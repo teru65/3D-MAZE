484173ce5d3c20f5f10995c369acd9861c7baca7
teru65 <teruma65zkkx4180@gmail.com>
2024-09-23 21:09:32 +0900

    add

--- Assets/holls.cs
+++ Assets/holls.cs
@@ -4,76 +4,61 @@ using UnityEditor;
 using UnityEngine;
 using UnityEngine.Jobs;
 
-
-public class holls : MonoBehaviour
+public class Holls : MonoBehaviour
 {
-
     public GameObject prefab;
-    private GameObject prefabInstance; // プレハブのインスタンスを保持
-                                       //インスタンス＝プレハブから生成された具体的なオブジェクト。
+    private List<GameObject> prefabInstances = new List<GameObject>(); // プレハブのインスタンスを保持
+
     public void ReceiveMessage()
     {
-        if (prefabInstance != null)
-        {
-            Destroy(prefabInstance);
-        }
+        // 既存のインスタンスを削除
+        RemoveAllPrefabs();
 
-        // goalcount が 1 のときにプレハブを生成
+        // goalcount に応じてプレハブを生成
         if (ball.goalcount == 1)
         {
-            if (prefab != null)
-            {
-                // ワールド座標を指定
-                Vector3 worldPosition = new Vector3(0.05f, 0.5f, 0.37f); // 任意の座標を指定
-                // プレハブをインスタンス化する
-                prefabInstance = Instantiate(prefab, worldPosition, Quaternion.identity);
-                // 親オブジェクト「maze」を設定する
-                Transform parentTransform = GameObject.Find("maze")?.transform;
-                if (parentTransform != null)
-                {
-                    prefabInstance.transform.SetParent(parentTransform, false); // ワールド座標を維持
-                }
-                // インスタンスの大きさを設定
-                prefabInstance.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
-            }
+            CreatePrefab(new Vector3(0.05f, 0.5f, 0.37f), new Vector3(0.5f, 0.5f, 0.5f));
         }
         else if (ball.goalcount == 2)
         {
-            if (prefab != null)
-            {
-                // ワールド座標を指定
-                Vector3 worldPosition = new Vector3(0.05f, 0.5f, 0.37f); // 任意の座標を指定
-                // プレハブをインスタンス化する
-                prefabInstance = Instantiate(prefab, worldPosition, Quaternion.identity);
-                // 親オブジェクト「maze」を設定する
-                Transform parentTransform = GameObject.Find("maze")?.transform;
-                if (parentTransform != null)
-                {
-                    prefabInstance.transform.SetParent(parentTransform, false); // ワールド座標を維持
-                }
-                // インスタンスの大きさを設定
-                prefabInstance.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
-
-
-                // ワールド座標を指定
-                Vector3 worldPosition1 = new Vector3(-0.2f, 0.5f, -0.17f); // 任意の座標を指定
-                // プレハブをインスタンス化する
-                prefabInstance = Instantiate(prefab, worldPosition1, Quaternion.identity);
-                // 親オブジェクト「maze」を設定する
-                Transform parentTransform1 = GameObject.Find("maze")?.transform;
-                if (parentTransform1 != null)
-                {
-                    prefabInstance.transform.SetParent(parentTransform1, false); // ワールド座標を維持
-                }
-                // インスタンスの大きさを設定
-                prefabInstance.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
-            }
+            CreatePrefab(new Vector3(0.05f, 0.5f, 0.37f), new Vector3(0.5f, 0.5f, 0.5f));
+            CreatePrefab(new Vector3(-0.2f, 0.5f, -0.17f), new Vector3(0.5f, 0.5f, 0.5f));
         }
+        else if (ball.goalcount == 3)
+        {
+            CreatePrefab(new Vector3(0.05f, 0.5f, 0.37f), new Vector3(0.5f, 0.5f, 0.5f));
+            CreatePrefab(new Vector3(-0.2f, 0.5f, -0.17f), new Vector3(0.5f, 0.5f, 0.5f));
+            CreatePrefab(new Vector3(-0.517f, 0.5f, 0.2f), new Vector3(0.5f, 0.5f, 0.5f));
+        }
+        else if (ball.goalcount == 4)
+        {
+            CreatePrefab(new Vector3(0.049f, 0.5f, -0.2f), new Vector3(3f, 3f, 3f));
+        }
+    }
 
+    private void CreatePrefab(Vector3 position, Vector3 scale)
+    {
+        // プレハブをインスタンス化する
+        GameObject prefabInstance = Instantiate(prefab, position, Quaternion.identity);
+        // 親オブジェクト「maze」を設定する
+        Transform parentTransform = GameObject.Find("maze")?.transform;
+        if (parentTransform != null)
+        {
+            prefabInstance.transform.SetParent(parentTransform, false); // ワールド座標を維持
+        }
+        // インスタンスの大きさを設定
+        prefabInstance.transform.localScale = scale;
 
+        // インスタンスをリストに追加
+        prefabInstances.Add(prefabInstance);
+    }
 
-
-
-
+    private void RemoveAllPrefabs()
+    {
+        foreach (GameObject instance in prefabInstances)
+        {
+            Destroy(instance);
+        }
+        prefabInstances.Clear(); // リストをクリア
     }
 }
